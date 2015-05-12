e007d215eb8be82aa2896bdc6c9d72390e709750
kozyarchuk <maksim_kozyarchuk@yahoo.com>
2015-05-12 11:23:21 -0400

    fix usage of tempfile

--- application.py
+++ application.py
@@ -1,11 +1,8 @@
 import flask
-import logging
 from nct.utils.db_logger import DBLogger
 from flask import request
 import boto
 from nct.utils.s3_message import S3Message
-import tempfile
-
 import sys, os, tempfile, logging
 from nct.apps.csv_trade_loader import CSVTradeLoader
 
@@ -27,15 +24,6 @@ logger.addHandler(handler)
 
 application = flask.Flask(__name__)
 conn = boto.connect_s3()
-#         return  self.conn.get_bucket(self.BUCKET)
-# 
-#     @property
-#     def conn(self):
-#         if not self._conn:
-#             self._
-#         return self._conn
-# #     @property
-
 
 @application.route('/',methods=['POST'])
 @application.route('/service-request',methods=['POST'])
@@ -46,13 +34,14 @@ def process_trade_file():
             trade_file_key = bucket.get_key(record.file_name)
             trade_file_url = trade_file_key.generate_url(0, query_auth=False, force_http=True)
             logger.info("TF URL: %s" % trade_file_url )
-            f, file_name = tempfile.mkstemp(".csv")
-            f.close()
+            fd, file_name = tempfile.mkstemp(".csv")
+            os.close(fd)
             urlretrieve(trade_file_url, file_name)
             logger.info("Uploading Trade File: %s" % file_name )
             loader = CSVTradeLoader.create_from_path(file_name)
             loader.run()
             logger.info("File uploaded")
+            os.remove(file_name)
     except Exception:
         logger.exception("Process Trade File Failed")
         
